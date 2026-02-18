cb27de1a000a0ec7fb35bed1a37214a47889af7e
Jean-Elie <jeaneliekubembula@gmail.com>
2026-02-18 08:28:15 +0100

    ajout du package SpeedInsights de Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const poppins = Poppins({ 
   weight: ['300', '400', '500', '600', '700', '800'],
   subsets: ['latin'],
